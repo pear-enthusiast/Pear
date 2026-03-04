4ce5e6123a78e58d5ca1f9baac976fa23d1f772c
pearman <Ampere102@protonmail.com>
2026-03-04 23:14:25 +0200

    Update testUI.lua

--- testUI.lua
+++ testUI.lua
@@ -1,2 +1,2 @@
 -- use pear btw
-loadstring(game:HttpGet("https://git.luau.software/Pear/public/raw/branch/main/obfuscated_PearUi-source.lua"))()
+return loadstring(game:HttpGet("https://git.luau.software/Pear/public/raw/branch/main/obfuscated_PearUi-source.lua"))()
