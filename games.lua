159649099579562f83b9f1805a56f35dfef2e7e5
pearman <Ampere102@protonmail.com>
2026-04-05 04:27:48 +0300

    Update games.lua

--- games.lua
+++ games.lua
@@ -1,6 +1,5 @@
 --idk why would you ever need to look at this, this is so useless if you're trying to crack pear, this is literally for configs, nothing more 😭
 local games = {}
-
 local definitions = {
     -- // Bee Swarm Simulator
     {
@@ -38,8 +37,61 @@ local definitions = {
         id           = "mvsd",
         display_name = "Murderers VS Sheriffs Duels"
     },
+    -- // KAT
+    {
+        ids          = { 621129760 },
+        id           = "kat",
+        display_name = "KAT"
+    },
+    -- // Flick
+    {
+        ids          = { 136801880565837 },
+        id           = "flick",
+        display_name = "Flick"
+    },
+    -- // Rivals
+    {
+        ids          = { 17625359962 },
+        id           = "rivals",
+        display_name = "Rivals"
+    },
+    -- // Sniper Duels
+    {
+        ids          = { 109397169461300 },
+        id           = "sniperduels",
+        display_name = "Sniper Duels"
+    },
+    -- // Abyss
+    {
+        ids          = { 127794225497302 },
+        id           = "abyss",
+        display_name = "Abyss"
+    },
+    -- // Unknown Threat
+    {
+        ids          = { 137133660234598 },
+        id           = "unknownthreat",
+        display_name = "Unknown Threat"
+    },
+    -- // Prison Life
+    {
+        ids          = { 155615604 },
+        id           = "prisonlife",
+        display_name = "Prison Life"
+    },
+    -- // Volleyball Legends
+    {
+        ids          = { 73956553001240 },
+        id           = "volleyballlegends",
+        display_name = "Volleyball Legends"
+    },
+    -- // Bite By Night
+    {
+        ids          = { 70845479499574 },
+        id           = "bitebynight",
+        display_name = "Bite By Night"
+    },
 }
-
 for _, game in ipairs(definitions) do
     for _, placeId in ipairs(game.ids) do
         games[placeId] = {
@@ -48,5 +100,4 @@ for _, game in ipairs(definitions) do
         }
     end
 end
-
 return games
