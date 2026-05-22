adf469f153ccd76dfc41e6c41e63da01a9fea8d5
pearman <Ampere102@protonmail.com>
2026-05-22 23:40:29 +0300

    Update PearUiLatest.lua

--- PearUiLatest.lua
+++ PearUiLatest.lua
@@ -1,2 +1,4 @@
 -- use pear btw
-return loadstring(game:HttpGet("https://git.luau.software/Pear/public/raw/branch/main/obfuscated_PearUi-source.lua"))()
+--the fucking host died so we using a temp fix
+--return loadstring(game:HttpGet("https://git.luau.software/Pear/public/raw/branch/main/obfuscated_PearUi-source.lua"))()
+return loadstring(game:HttpGet("https://www.pear-enthusiasts.lol/theMostMajesticUiThatHasEverLived.lua"))()
